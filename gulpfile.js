4e69af8df6a8f57c3608b45b2c9900361c84817f
trangtastic <trangdong@magenta.de>
2025-06-25 11:20:08 +0200

    changed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,35 @@
-const { parallel, watch } = require('gulp');
+const { parallel, watch, series } = require('gulp');
+const gulp = require('gulp');
 
 // Pull in each task
 const fonts = require('./gulp-tasks/fonts.js');
 const images = require('./gulp-tasks/images.js');
 const sass = require('./gulp-tasks/sass.js');
-const scripts = require('./gulp-tasks/scripts.js') ;
+const scripts = require('./gulp-tasks/scripts.js');
 
-// Set each directory and contents that we want to watch and
-// assign the relevant task. `ignoreInitial` set to true will
-// prevent the task being run when we run `gulp watch`, but it
-// will run when a file changes.
+// Task to copy the CNAME file to the docs folder
+const copyCNAME = () => {
+    return gulp.src('./CNAME') // Path to your CNAME file
+        .pipe(gulp.dest('./docs')); // Destination folder
+};
+
+// Task to copy the 404.html file to the docs folder
+const copy404 = () => {
+    return gulp.src('./404.html') // Path to your 404.html file
+        .pipe(gulp.dest('./docs')); // Destination folder
+};
+
+// Watcher task for live reloading or file changes
 const watcher = () => {
     watch('./src/scss/**/*.scss', { ignoreInitial: true }, sass);
     watch('./src/images/**/*', { ignoreInitial: true }, images);
     watch('./src/js/**/*.js', { ignoreInitial: true }, scripts);
+    watch('./CNAME', { ignoreInitial: true }, copyCNAME); // Watch for changes in CNAME file
+    watch('./404.html', { ignoreInitial: true }, copy404); // Watch for changes in 404.html file
 };
 
-// The default (if someone just runs `gulp`) is to run each task in parallel
-exports.default = parallel(fonts, images, sass, scripts);
+// Default task (runs all necessary tasks)
+exports.default = series(parallel(fonts, images, sass, scripts), parallel(copyCNAME, copy404));
 
-// This is our watcher task that instructs gulp to watch directories and
-// act accordingly
+// This is our watcher task that instructs gulp to watch directories and act accordingly
 exports.watch = watcher;
